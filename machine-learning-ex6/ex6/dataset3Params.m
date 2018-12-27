60bb3f8282ad97af55491d4ea2c8e3b32d857ab0
Paulo Soares <phsoares.ita@gmail.com>
2018-12-27 20:34:25 -0200

    svm with gaussian kernel

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -23,7 +23,21 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+minError = 1000000;
+for c = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+	for s = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+		model = svmTrain(X, y, c, @(x1, x2) gaussianKernel(x1, x2, s));
+		predictions = svmPredict(model, Xval);
+		e = mean(double(predictions ~= yval));
+		if (e < minError)
+			minError = e;
+			C = c;
+			sigma = s;
+		endif
+	endfor
+endfor
 
+% 
 
 
 
